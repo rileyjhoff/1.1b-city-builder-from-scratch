7424a3b462e7f5c4aa7b08cc984a66de4c7dfd92
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-11 07:12:23 -0800

    initialized state and grabbed DOM elements for the please select options from dropdowns

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const topDropdown = document.getElementById('dropdown-1');
 const middleDropdown = document.getElementById('dropdown-2');
 const bottomDropdown = document.getElementById('dropdown-3');
 
+const topPleaseSelect = document.getElementById('select-1');
+const middlePleaseSelect = document.getElementById('select-2');
+const bottomPleaseSelect = document.getElementById('select-3');
+
 const topCountEl = document.getElementById('top');
 const middleCountEl = document.getElementById('middle');
 const bottomCountEl = document.getElementById('bottom');
@@ -19,6 +23,13 @@ const sloganAreaEl = document.getElementById('slogan-list-div');
 console.log(topImage, topDropdown, topCountEl, middleImage, middleDropdown, middleCountEl, bottomImage, bottomDropdown, bottomCountEl, sloganInput, sloganButton, sloganAreaEl);
 
 // let state
+top = 0;
+middle = 0;
+bottom = 0;
+const slogans = [];
+topPleaseSelect.disabled = true;
+middlePleaseSelect.disabled = true;
+bottomPleaseSelect.disabled = true;
 
 // set event listeners 
   // get user input
