e0603cc427cf68c1303cb1f012b70565ae08af31
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-10 17:59:56 -0800

    grabbed DOM elements

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 // import functions and grab DOM elements
+const topImage = document.getElementById('image-1');
+const middleImage = document.getElementById('image-2');
+const bottomImage = document.getElementById('image-3');
+
+const topDropdown = document.getElementById('dropdown-1');
+const middleDropdown = document.getElementById('dropdown-2');
+const bottomDropdown = document.getElementById('dropdown-3');
+
+const topCountEl = document.getElementById('top');
+const middleCountEl = document.getElementById('middle');
+const bottomCountEl = document.getElementById('bottom');
+
+const sloganInput = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
+
+const sloganAreaEl = document.getElementById('slogan-list-div');
+
+console.log(topImage, topDropdown, topCountEl, middleImage, middleDropdown, middleCountEl, bottomImage, bottomDropdown, bottomCountEl, sloganInput, sloganButton, sloganAreaEl);
 
 // let state
 
